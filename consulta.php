6ebe83617db5d512d46463c31fe1741af69fa39b
Duda <dudacal20@gmail.com>
2024-08-29 15:56:23 -0300

    Segunda versão

--- consulta.php
+++ consulta.php
@@ -1,29 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>loja</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
-</head>
-<body>
+<?php
+    include "conecta.php";
+?>
+
+
+
 
 <?php
- include "conect.php";
- $sql = "SELECT id, descricao, fabricante, qnt, preco_custo, preco_venda, imagem FROM produtos";
- $resultado = mysqli_query($conexao, $sql);
- echo "<br>";
- if (mysqli_num_rows($resultado) > 0){
-    while ($registro = mysqli_fetch_assoc($resultado)){
-        echo "<div class=dados>". "id: " . $registro["id"]. " descricao: " . $registro["descricao"].
-        " fabricante: ". $registro["fabricante"]. "qnt". $registro["qnt"].
-        "preco_venda:" . $registro["preco_venda"]. "imagem:". $registro["imagem"]. "<br>";
-
+    echo "<b>Mostrando todos os registros</b><br>";
+    $sql = "SELECT id, descricao, fabricante, qnt, preco_custo, preco_venda FROM produtos";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+  
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)) {
+        echo "id: " . $registro["id"]. " - Descrição: " . $registro["descricao"].
+        " - Fabricante:" . $registro["fabricante"]. " - Quantidade:" . $registro["qnt"].
+        " - Preço de custo:" . $registro["preco_custo"]. " - Preço de venda:" . $registro["preco_venda"].
+        "<br>";
+        }
+    } else{
+        echo "Nenhum registro encontrado.";
     }
- } else{
-    echo "Nenhum registro encontrado!";
 
- }
- ?>
- </body>
-</html>
+    
+?>
